01a8a8af8c4c1164e86a3a63b9a1f7b3ff7de350
alfarroqie <48423485+alfarroqie@users.noreply.github.com>
2021-01-06 09:43:08 +0700

    add analytic

--- src/main.js
+++ src/main.js
@@ -1,8 +1,14 @@
 import Vue from 'vue'
-//  import App from './components/Cms.vue'
+// import App from './components/Cms.vue'
 import App from './App.vue'
 import router from './router'
 import vuetify from './plugins/vuetify';
+import VueAnalytics from 'vue-analytics'
+
+Vue.use(VueAnalytics, {
+  id: 'UA-186540839-1',
+  router
+})
 
 Vue.config.productionTip = false
 
