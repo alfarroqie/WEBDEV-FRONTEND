ce954c058d36f9d90b7dc3872ef7f956d162711b
ichacahyawulan <45441252+ichacahyawulan@users.noreply.github.com>
2021-02-14 00:04:14 +0700

    benerin tampilan login signup

--- src/components/Login2.vue
+++ src/components/Login2.vue
@@ -1,31 +1,50 @@
 <template>
-    <div class="vue-template">
-        <v-card class="event-card"> 
-            <v-list align = "center">
-                <form>
-                    <h3 class="text-center">Login</h3>
+  <div class="vue-template">
+    <v-card class="event-card">
+      <v-list align="left">
+        <form>
+          <h3 class="text-center">Login</h3>
 
-                    <div class="form-group">
-                        <label>email</label>
-                        <input type="email" class="form-control form-control-lg" v-model="emailUser" required />
-                    </div>
+          <div class="form-group">
+            <label>E-mail</label>
+            <input
+              type="email"
+              class="form-control form-control-lg"
+              v-model="emailUser"
+              required
+            />
+          </div>
 
-                    <div class="form-group">
-                        <label>Password</label>
-                        <input type="password" class="form-control form-control-lg" v-model="passwordUser" required />
-                    </div>
+          <div class="form-group">
+            <label>Password</label>
+            <input
+              type="password"
+              class="form-control form-control-lg"
+              v-model="passwordUser"
+              required
+            />
+          </div>
 
-                    <RouterLink :to="'/login'">
-                    <a href=""> <button type="submit" class="btn btn-primary btn-lg btn-block" @click="loginUser">Login</button> </a>
-                    </RouterLink>
-                    <p class="forgot-account text-right">
-                        Dont have account?
-                        <router-link :to="'/signup'">Sign Up</router-link>
-                    </p>
-                </form>
-            </v-list>
-        </v-card>
-    </div>
+          <RouterLink :to="'/login'">
+            <a href="">
+              <button
+                type="submit"
+                class="btn btn-primary btn-lg btn-block"
+                style="color: white"
+                @click="loginUser"
+              >
+                Login
+              </button>
+            </a>
+          </RouterLink>
+          <p class="forgot-account text-right" style="padding-top: 10px">
+            Don't have account yet?
+            <router-link :to="'/signup'">Sign Up</router-link>
+          </p>
+        </form>
+      </v-list>
+    </v-card>
+  </div>
 </template>
 
 <script>
@@ -33,55 +52,56 @@
 import UserService from "../services/UserDataService";
 import ErrorDialog from "./ErrorDialog.vue";
 export default {
-    component:{
-        ErrorDialog,
-    },
-    data() {
-        return {
-            emailUser : "",
-            passwordUser : "",
-        }
-    },
-    methods:{
-      loginUser : function(){
-          const users = {
-            email: this.emailUser,
-            password: this.passwordUser,
-          }
+  component: {
+    ErrorDialog,
+  },
+  data() {
+    return {
+      emailUser: "",
+      passwordUser: "",
+    };
+  },
+  methods: {
+    loginUser: function () {
+      const users = {
+        email: this.emailUser,
+        password: this.passwordUser,
+      };
 
-          this.isLoading = true;
-          UserService.login(users)
-              .then(result => {
-                this.isLoading = false;
-                // console.log(result);
-                localStorage.removeItem('user');
-                localStorage.setItem('user', JSON.stringify(result.data));
-                const datauser = result.data.dataUser;
-                // console.log("Authentication: ",result.data);
-                if(datauser.isAdmin){
-                    this.$router.push("/admin");    
-                }
-                else{
-                    this.$router.push("/");
-                }
-                })
-              .catch(err => {
-                this.isLoading = false;
-                this.isError = true;
-                console.log(err)})
-        }
-    }
-}
+      this.isLoading = true;
+      UserService.login(users)
+        .then((result) => {
+          this.isLoading = false;
+          // console.log(result);
+          localStorage.removeItem("user");
+          localStorage.setItem("user", JSON.stringify(result.data));
+          const datauser = result.data.dataUser;
+          // console.log("Authentication: ",result.data);
+          if (datauser.isAdmin) {
+            this.$router.push("/admin");
+          } else {
+            this.$router.push("/");
+          }
+        })
+        .catch((err) => {
+          this.isLoading = false;
+          this.isError = true;
+          console.log(err);
+        });
+    },
+  },
+};
 </script>
 
 <style>
 .event-card {
   overflow: hidden;
-  width: 50%;
+  width: 500px;
   margin: 60px auto auto;
+  padding: 20px;
   border-radius: 0.3em;
 }
 body {
-    margin-top: 50px;
+  margin-top: 50px;
 }
 </style>
--- src/components/SignUp.vue
+++ src/components/SignUp.vue
@@ -1,100 +1,142 @@
 <template>
-    <div class="vue-template">
+  <div class="vue-template">
+    <v-card class="event-card">
+      <v-list align="left">
         <form>
-            <h3 class="text-center">Sign Up</h3>
+          <h3 class="text-center">Sign Up</h3>
 
-            <div class="form-group">
-                <label>Full Name</label>
-                <input type="text" class="form-control form-control-lg" v-model="fullnameUser" required />
-            </div>
+          <div class="form-group">
+            <label>Full Name</label>
+            <input
+              type="text"
+              class="form-control form-control-lg"
+              v-model="fullnameUser"
+              required
+            />
+          </div>
 
-            <div class="form-group">
-                <label>Username</label>
-                <input type="username" class="form-control form-control-lg" v-model="userName" required />
-            </div>
+          <div class="form-group">
+            <label>Username</label>
+            <input
+              type="username"
+              class="form-control form-control-lg"
+              v-model="userName"
+              required
+            />
+          </div>
 
-            <div class="form-group">
-                <label>email</label>
-                <input type="email" class="form-control form-control-lg" v-model="emailUser" required />
-            </div>
+          <div class="form-group">
+            <label>email</label>
+            <input
+              type="email"
+              class="form-control form-control-lg"
+              v-model="emailUser"
+              required
+            />
+          </div>
 
-            <div class="form-group">
-                <label>Password</label>
-                <input type="password" class="form-control form-control-lg" v-model="passwordUser" required />
-            </div>
-            <RouterLink :to="'/signup'">
-                <a href="#"> <button type="submit" class="btn btn-primary btn-lg btn-block" @click="addUser">Sign Up</button> </a>
-            </RouterLink>
-            <p class="forgot-account text-right">
-                Already registered?
-                <router-link :to="'/login'">Log in</router-link>
-            </p>
+          <div class="form-group">
+            <label>Password</label>
+            <input
+              type="password"
+              class="form-control form-control-lg"
+              v-model="passwordUser"
+              required
+            />
+          </div>
+          <RouterLink :to="'/signup'">
+            <a href="#">
+              <button
+                type="submit"
+                class="btn btn-primary btn-lg btn-block"
+                style="color: white"
+                @click="addUser"
+              >
+                Sign Up
+              </button>
+            </a>
+          </RouterLink>
+          <p class="forgot-account text-right" style="padding-top: 10px">
+            Already have an account?
+            <router-link :to="'/login'">Log in</router-link>
+          </p>
         </form>
-    </div>
+      </v-list>
+    </v-card>
+  </div>
 </template>
 
 <script>
 import UserService from "../services/UserDataService";
 import ErrorDialog from "./ErrorDialog.vue";
 export default {
-    component:{
-        ErrorDialog,
-    },
-    data() {
-        return {
-            fullnameUser : "",
-            userName : "",
-            emailUser : "",
-            passwordUser : "",
-        }
-    },
-    methods:{
-      addUser : function(){
-          const users = {
-            name: this.fullnameUser,
-            username: this.userName,
-            email: this.emailUser,
-            password: this.passwordUser,
-            isAdmin: false,
-          }
+  component: {
+    ErrorDialog,
+  },
+  data() {
+    return {
+      fullnameUser: "",
+      userName: "",
+      emailUser: "",
+      passwordUser: "",
+    };
+  },
+  methods: {
+    addUser: function () {
+      const users = {
+        name: this.fullnameUser,
+        username: this.userName,
+        email: this.emailUser,
+        password: this.passwordUser,
+        isAdmin: false,
+      };
 
-          const log = {
-                email: this.emailUser,
-                password:this.passwordUser
-            }
+      const log = {
+        email: this.emailUser,
+        password: this.passwordUser,
+      };
 
-          this.isLoading = true;
-          UserService.create(users)
-              .then(result => {
-                this.isLoading = false;
-                console.log(result);
-                    UserService.login(log)
-                    .then(result => {
-                        this.isLoading = false;
-                        console.log(result);
-                        localStorage.removeItem('user');
-                        localStorage.setItem('user', JSON.stringify(result.data));
-                        console.log("Authentication: ",result.data);
-                        this.$router.push("/");
-                    })
-                    .catch(err => {
-                        this.isLoading = false;
-                        this.isError = true;
-                        console.log(err)})
-                        // this.$router.push("/login");
-                        // location.reload()
-                    })
-              .catch(err => {
-                this.isLoading = false;
-                this.isError = true;
-                console.log(err)})
-        }
-    }
-}
+      this.isLoading = true;
+      UserService.create(users)
+        .then((result) => {
+          this.isLoading = false;
+          console.log(result);
+          UserService.login(log)
+            .then((result) => {
+              this.isLoading = false;
+              console.log(result);
+              localStorage.removeItem("user");
+              localStorage.setItem("user", JSON.stringify(result.data));
+              console.log("Authentication: ", result.data);
+              this.$router.push("/");
+            })
+            .catch((err) => {
+              this.isLoading = false;
+              this.isError = true;
+              console.log(err);
+            });
+          // this.$router.push("/login");
+          // location.reload()
+        })
+        .catch((err) => {
+          this.isLoading = false;
+          this.isError = true;
+          console.log(err);
+        });
+    },
+  },
+};
 </script>
 
 <style>
+.event-card {
+  overflow: hidden;
+  width: 500px;
+  margin: 60px auto auto;
+  padding: 20px;
+  border-radius: 0.3em;
+}
 body {
-    margin-top: 50px;
+  margin-top: 50px;
 }
 </style>
